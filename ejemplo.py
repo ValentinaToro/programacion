eb8c490a5b4913cc2201ae975f64e97e84b335f9
unknown <zvaletoro@gmail.com>
2020-08-11 22:42:38 -0400

    ejemplo python

---
+++ ejemplo.py
@@ -0,0 +1,29 @@
+def imprimirVector(indice):
+    print("imprimo valor elemento ", mi_array[indice])
+
+# str (texto) int (enteros) float/double (decimales) bool (verdadero/falso  0/1)
+variable = not True
+resultado = 1
+
+#arrays, arreglos, vectores
+mi_array = [1,'a',3,4,5,6,7,8,9,10]
+
+if variable:
+    #ejecuta todo el codigo que yo coloque aca si la condicion es verdadera
+    resultado = 2
+else:
+    resultado = True
+
+
+
+
+print("hola")
+
+for x in range(0, 10):
+    resultado = False
+print(type(resultado), resultado)
+
+imprimirVector(3)
+
+mi_array[0], mi_array['nombre']
+
